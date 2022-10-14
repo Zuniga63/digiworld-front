8143b0eda16ba7100fcc91f0d946970a854cf353
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-10-14 13:05:10 -0500

    add default base URL to axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
 
 import './assets/main.css';
 
 const app = createApp(App);
+axios.defaults.baseURL = import.meta.env.VITE_BACK_END_URL;
 
 app.use(router);
 
