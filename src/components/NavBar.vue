0448afb030841ea6c196bdc3900a85f953bd0988
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-10-14 23:37:49 -0500

    add home page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+    role: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push('/login');
+    },
+  },
+};
+</script>
+<template>
+  <nav class="border-b border-gray-100 bg-white">
+    <!-- Primary Navigation Menu -->
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        <!-- Logo -->
+        <div class="flex w-24 shrink-0 items-center lg:w-40">
+          <img
+            src="https://www.digiworld.com.co/assetsInicio/img/iconL.svg"
+            alt="logo"
+          />
+        </div>
+
+        <div class="hidden sm:block">
+          <h2 class="text-center text-xl font-bold text-gray-800">
+            {{ user?.name }}
+          </h2>
+          <p class="text-center text-sm font-bold italic text-gray-700">
+            {{ role?.name }}
+          </p>
+        </div>
+
+        <button
+          class="rounded border border-red-600 bg-red-500 px-4 py-2 font-bold text-white shadow shadow-red-400 transition-opacity hover:opacity-80 active:opacity-100"
+          @click="logout"
+        >
+          Log out
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,32 @@
+<script>
+import TaskListItem from './TaskListItem.vue';
+export default {
+  props: {
+    tasks: Object,
+    users: Object,
+    isProfessor: Boolean,
+  },
+  components: { TaskListItem },
+};
+</script>
+<template>
+  <div class="shadow-lg shadow-slate-900">
+    <h3
+      class="rounded-t border border-slate-800 bg-slate-800 px-4 py-2 text-center font-bold text-gray-200"
+    >
+      Task List
+    </h3>
+
+    <div
+      class="grid max-h-96 gap-3 overflow-y-scroll rounded-b border-x border-b border-slate-800 px-4 py-2"
+    >
+      <TaskListItem
+        v-for="task in tasks"
+        :key="task._id"
+        :task="task"
+        :users="users"
+        :is-professor="isProfessor"
+      />
+    </div>
+  </div>
+</template>
